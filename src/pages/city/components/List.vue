1fe3307365f100e812b91450b9ba40703f4b4648
ickt_17_周帅帅 <18255823842@163.com>
2019-11-21 16:20:45 +0800

    'finish-city-css'

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,134 @@
+<template>
+    <div class="list" ref="wrapper">
+       <div>
+           <div class="area">
+               <div class="title">当前城市</div>
+               <div class="button-list">
+                   <div class="button-wrapper">
+                       <div class="button">北京</div>
+                   </div>
+               </div>
+           </div>
+           <div class="area">
+               <div class="title">当前城市</div>
+               <div class="button-list">
+                   <div class="button-wrapper">
+                       <div class="button">北京</div>
+                   </div>
+                   <div class="button-wrapper">
+                       <div class="button">北京</div>
+                   </div>
+                   <div class="button-wrapper">
+                       <div class="button">北京</div>
+                   </div>
+                   <div class="button-wrapper">
+                       <div class="button">北京</div>
+                   </div>
+               </div>
+           </div>
+           <div class="area">
+               <div class="title">A</div>
+               <div class="item-list">
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+               </div>
+           </div>
+           <div class="area">
+               <div class="title">A</div>
+               <div class="item-list">
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+               </div>
+           </div>
+           <div class="area">
+               <div class="title">A</div>
+               <div class="item-list">
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+                   <div class="item border-bottom">
+                       阿坝藏族羌族自治州
+                   </div>
+               </div>
+           </div>
+       </div>
+    </div>
+</template>
+
+<script>
+	import BScroll from 'better-scroll'
+
+	export default {
+		name: 'CityList',
+        mounted() {
+			this.scroll = new BScroll(this.$refs.wrapper)
+		}
+	}
+</script>
+
+<style scoped lang="stylus">
+    @import '~styles/varibles.styl'
+    .border-topbottom
+        &:before
+            border-color: #ccc
+        &:after
+            border-color: #ccc
+    .border-bottm
+        &:beforee
+            border-color: #ccc
+    .list
+        overflow: hidden
+        position: absolute
+        top: 1.58rem
+        left: 0
+        right: 0
+        bottom: 0
+        .title
+            line-height: .54rem
+            background-color: #eee
+            padding-left: .2rem
+            color: #666
+            font-size: .26rem
+        .button-list
+            overflow: hidden
+            padding: .1rem .6rem .1rem .1rem
+            .button-wrapper
+                width: 33.33%
+                float: left
+                .button
+                    text-align: center
+                    margin: .1rem
+                    padding: .1rem 0
+                    border: .02rem solid #ccc
+                    border-radius: .06rem
+        .item-list
+            .item
+                line-height: .76rem
+                padding-left: .2rem
+</style>
